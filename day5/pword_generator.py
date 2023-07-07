525c16ea2fa57a2df23a9c49ea8909ed8cb0b875
John Paul <rangeljohnpaul@gmail.com>
2023-07-07 11:25:00 +0800

    added shorter alternate solution

--- day5/pword_generator.py
+++ day5/pword_generator.py
@@ -10,27 +10,48 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-# 2 - generate a random letter/symbol/number for the password
-# 2a - make a list to save the generated random l/s/n
-random_letters = []
-random_symbols = []
-random_numbers = []
-# 2b - generate random l/s/n using choice()
-for l in range(0, nr_letters):
-    random_letters.append(random.choice(letters))
-for s in range(0, nr_symbols):
-    random_symbols.append(random.choice(symbols))
-for n in range(0, nr_numbers):
-    random_numbers.append(random.choice(numbers))
-# 2c - combine all the generated l/s/n into one list
-password = random_letters + random_numbers + random_symbols
-# 3 display the generated random password (unshuffled) as a string using a delimiter
-delimiter = ''
-unshuffled_pw = delimiter.join(password)
-print(unshuffled_pw)
-# 4 - shuffle the list to create the final password and print result
-random.shuffle(password)
-final_password = delimiter.join(password)
-print(final_password)
+# # 2 - generate a random letter/symbol/number for the password
+# # 2a - make a list to save the generated random l/s/n
+# random_letters = []
+# random_symbols = []
+# random_numbers = []
+# # 2b - generate random l/s/n using choice()
+# for l in range(0, nr_letters):
+#     random_letters.append(random.choice(letters))
+# for s in range(0, nr_symbols):
+#     random_symbols.append(random.choice(symbols))
+# for n in range(0, nr_numbers):
+#     random_numbers.append(random.choice(numbers))
+# # 2c - combine all the generated l/s/n into one list
+# password = random_letters + random_numbers + random_symbols
+# # 3 display the generated random password (unshuffled) as a string using a delimiter
+# delimiter = ''
+# unshuffled_pw = delimiter.join(password)
+# print(unshuffled_pw)
+# # 4 - shuffle the list to create the final password and print result
+# random.shuffle(password)
+# final_password = delimiter.join(password)
+# print(final_password)
 
+# alternate solution
+# 2a - make a list to save all the generated random l/s/n
+pword = []
+# 2b - generate random l/s/n using choice()
+for char in range(0, nr_letters):
+    pword.append(random.choice(letters))
+for char in range(0, nr_symbols):
+    pword.append(random.choice(symbols))
+for char in range(0, nr_numbers):
+    pword.append(random.choice(numbers))
+unshuffled_pword = ''
+for char in pword:
+    unshuffled_pword += char
+print(unshuffled_pword)
+# 2c - shuffle the items on the list
+random.shuffle(pword)
+# 3 - transform the list to a string then print result
+final_pword = ''
+for char in pword:
+    final_pword += char
+print(f"Shuffled password: {final_pword}")
 
