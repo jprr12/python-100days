8746530f720036f923c34fc548431ff3e178024c
John Paul <rangeljohnpaul@gmail.com>
2023-07-10 11:33:27 +0800

    day 6 - code blocks, functions, while loops ft. reeborg's world

---
+++ day6/script.py
@@ -0,0 +1,20 @@
+# code blocks, functions, while loops & karel
+
+# # defining/calling functions
+# # function that will print hello when called
+# def some_function():
+#     print("hello")
+# # calling the function
+# some_function()
+
+# while loop
+# while condition_is_true:
+#   do_this()
+chips = 6
+while chips > 0: # should print 'eat chips' 6 times
+    print("eat chips")
+    chips -= 1
+
+# check reeborg's world for more while-loop exercises
+# final project: maze (reeborg)
+
